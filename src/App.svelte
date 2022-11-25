6db6d43d268f55c5602eb37114724e67e48c0c49
matteoscaringi <scaringi.matteo@icloud.com>
2022-11-25 22:57:10 +0100

    finished "Main.svelte"

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "./app.css";
+
+  import Main from "./lib/Main.svelte";
+</script>
+
+<header class="h-screen bg-gradient-to-tr from-[#3b5292] to-[#8a96b4]">
+  <Main/>
+</header>
+
