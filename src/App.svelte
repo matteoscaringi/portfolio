2b973c4b45f14c2a3d8fdd129bfc5274e15cced6
matteoscaringi <scaringi.matteo@icloud.com>
2022-11-26 13:48:31 +0100

    added dummy WIP project page

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,13 @@
   import "./app.css";
 
   import Main from "./lib/Main.svelte";
+  import Projects from "./lib/Projects.svelte";
 </script>
 
-<header class="h-screen bg-gradient-to-tr from-[#3b5292] to-[#8a96b4]">
+<header class="h-auto bg-gradient-to-tr from-[#3b5292] to-[#8a96b4]">
   <Main/>
+  <Projects/>
 </header>
+
+
 
---
+++ src/lib/Projects.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+</script>
+
+<header class="h-screen">
+    <div class="flex flex-col justify-center align-middle text-center h-screen">
+        <h1 class="font font-bold item top">My work</h1>
+        <h1 class="font font-semibold">Comming soon!</h1>
+    </div>
+</header>
