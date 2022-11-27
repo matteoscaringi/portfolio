7800ffc7ee7c936fe4933a6ab865cf2ab111cfbd
matteoscaringi <scaringi.matteo@icloud.com>
2022-11-27 14:19:08 +0100

    removed unnesecary code for srolling

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -2,18 +2,6 @@
     import "../app.css";
     import Icon from "@iconify/svelte";
     import PFP from "../../public/PFP.jpg";
-
-    function scrollTo(element, to, duration) {
-        if (duration <= 0) return;
-        var difference = to - element.scrollTop;
-        var perTick = (difference / duration) * 10;
-
-        setTimeout(function () {
-            element.scrollTop = element.scrollTop + perTick;
-            if (element.scrollTop === to) return;
-            scrollTo(element, to, duration - 10);
-        }, 10);
-    }
 </script>
 
 <header class="h-screen">
